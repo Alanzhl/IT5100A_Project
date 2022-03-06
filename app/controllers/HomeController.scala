9cf389e55c749c3255b2ad9ffdec2d678b4da109
zstcode <17093158+zstcode@users.noreply.github.com>
2022-03-07 00:50:44 +0800

    add routes

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -9,13 +9,93 @@ import play.api.libs.json.Json
 class HomeController @Inject()(val controllerComponents: ControllerComponents)
   extends BaseController {
 
-  def index = Action { request: Request[AnyContent] =>
-    Ok("Hello World")
-  }
-
-  def getJSON = Action { request: Request[AnyContent] =>
+  // index: return a list of available time slots
+  def index: Action[AnyContent] = Action { request: Request[AnyContent] =>
     Ok(Json.toJson(
       Map("message" -> "Hello")
+    ))
+  }
+
+  def login: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  def logout: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  def register: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  // bookTimeslot: user book a time slot
+  def bookTimeslot: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  // cancelTimeslot: user cancel a booking
+  def cancelTimeslot: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  //queryUserBookings: get user's current bookings
+  def queryUserBookings: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  // editUserBooking: edit time slot for a booking
+  def editUserBooking: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+
+  // queryBookingRecords: staffs query bookings in a time slot
+  def queryBookingRecords: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+  // addTimeslot: staffs add a timeslot
+  def addTimeslot: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+  // deleteTimeslot: staffs delete a timeslot
+  def deleteTimeslot: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+  // editTimeslot: staffs edit a timeslot
+  def editTimeslot: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+  // markBookingStatus: staffs mark for booking records status
+  def markBookingStatus: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
+    ))
+  }
+  // getStatistics: staffs get statistics of bookings in a week
+  def getStatistics: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    Ok(Json.toJson(
+      Map("success" -> 0)
     ))
   }
 }

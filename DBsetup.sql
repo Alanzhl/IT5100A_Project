c4c11fc4d10984e7b35e0b5b5f91f6b70277413e
zstcode <17093158+zstcode@users.noreply.github.com>
2022-03-07 23:35:46 +0800

    fix db script error

--- DBsetup.sql
+++ DBsetup.sql
@@ -2,6 +2,6 @@
 -- we have chosen PostgreSQL for our project, 
 -- so the script can run under the root user (probably "postgres") in psql
 
-CREATE USER dev WITH PASSWORD "password";
+CREATE USER dev WITH PASSWORD 'password';
 
 CREATE DATABASE it5100a_proj WITH OWNER=dev;

8451e3767157cb54a6a6ca8b04bc0ff4b6d75062
Haoliang Zou <zouhaoliang@bupt.edu.cn>
2022-03-08 11:46:16 +0800

    Modified some bugs.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -112,11 +112,11 @@ class HomeController @Inject()(protected val dbConfigProvider: DatabaseConfigPro
 
   // initialize the schemas of tables
   def createTables: Action[AnyContent] = Action.async { _ =>
-    model.init().map(_ => Ok("Tables initiated."))
+    model.init().map(info => Ok(info))
   }
 
   // (one-time insertion) insert sample data to the tables for testing purpose
   def insertSamples(): Action[AnyContent] = Action.async { _ =>
-    model.insertSamples().map(_ => Ok("Inserted sample with 4 slots, 3 users and 5 bookings."))
+    model.insertSamples().map(info => Ok(info))
   }
 }
--- app/models/Tables.scala
+++ app/models/Tables.scala
@@ -12,32 +12,35 @@ object Tables extends {
 
 // schemas of the tables
 class Tables {
-  class Slots(tag: Tag) extends Table[(Int, Timestamp, Timestamp, Int, Short)](tag, "SLOTS") {
-    def slotID = column[Int]("slot_id", O.PrimaryKey, O.AutoInc)
+  case class Slot(slotID:Int, startAt:Timestamp, endAt:Timestamp, vacancy:Int, status:Short)
+  class Slots(tag: Tag) extends Table[Slot](tag, "SLOTS") {
+    def slotID = column[Int]("slot_id", O.AutoInc, O.PrimaryKey)
     def startAt = column[Timestamp]("start_at")
     def endAt = column[Timestamp]("end_at")
     def vacancy = column[Int]("vacancy")
     def status = column[Short]("status")
-    def * = (slotID, startAt, endAt, vacancy, status)
+    def * = (slotID, startAt, endAt, vacancy, status) <> (Slot.tupled, Slot.unapply)
   }
   lazy val slots = TableQuery[Slots]
 
-  class Users(tag: Tag) extends Table[(Int, String, String, String, String)](tag, "USERS") {
-    def userID = column[Int]("user_id", O.PrimaryKey, O.AutoInc)
+  case class User(userID:Int, identifier:String, name:String, email:String, password:String)
+  class Users(tag: Tag) extends Table[User](tag, "USERS") {
+    def userID = column[Int]("user_id", O.AutoInc, O.PrimaryKey)
     def identifier = column[String]("identifier", O.Unique)
     def name = column[String]("name")
     def email = column[String]("email", O.Unique)
     def password = column[String]("password")
-    def * = (userID, identifier, name, email, password)
+    def * = (userID, identifier, name, email, password) <> (User.tupled, User.unapply)
   }
   lazy val users = TableQuery[Users]
 
-  class Bookings(tag: Tag) extends Table[(Int, Short, Int, Int)](tag, "BOOKINGS") {
-    def bookingID = column[Int]("booking_id", O.PrimaryKey, O.AutoInc)
+  case class Booking(bookingID:Int, status:Short, userID:Int, slotID:Int)
+  class Bookings(tag: Tag) extends Table[Booking](tag, "BOOKINGS") {
+    def bookingID = column[Int]("booking_id", O.AutoInc, O.PrimaryKey)
     def status = column[Short]("status")
     def userID = column[Int]("user_id")
     def slotID = column[Int]("slot_id")
-    def * = (bookingID, status, userID, slotID)
+    def * = (bookingID, status, userID, slotID) <> (Booking.tupled, Booking.unapply)
 
     def user = foreignKey(
       "user_fk", userID, users)(
